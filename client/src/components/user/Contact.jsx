e4b773359ee770a53485ca54328696cc6511bc54
Irs444 <irs786had123@gmail.com>
2024-03-24 23:31:03 +0530

    Add contact

--- client/src/components/user/Contact.jsx
+++ client/src/components/user/Contact.jsx
@@ -4,7 +4,14 @@ const Contact = () => {
   return (
     <div>
      
-
+    <div className="container">
+      <div className="row">
+        <div className="col-md-5">
+          <label htmlFor="" className="form-label">Name</label>
+          <input type="text" className="form-control" />
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
